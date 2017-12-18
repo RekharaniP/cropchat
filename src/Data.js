d6830fb4d6c23544f90beac8d5e441cac9dae83d
Rekharani P <914442@uk.ema.ad.pwcinternal.com>
2017-12-18 13:26:37 +0000

    First commit

---
+++ src/Data.js
@@ -0,0 +1,45 @@
+export default {
+    
+        
+    testthis:function(){
+        
+        picturesss : 'looooollll';
+        return  [
+        {
+            'id': 0,
+            'url': 'https://25.media.tumblr.com/tumblr_m40h4ksiUa1qbyxr0o1_400.gif',
+            'comments':'A cat game',
+            'info':'Posted by Kevin on friday'
+        },
+        {
+            'id':1,
+            'url':'https://25.media.tumblr.com/tumblr_lhd7n9Qec01qgnva2o1_500.jpg',
+            'comments':'Tatoo and Cat',
+            'info':'Posted by Charles on Tuesday'
+        },
+        {
+            'id':2,
+            'url':'https://24.media.tumblr.com/tumblr_m4j2atctRm1qejbiro1_1280.jpg',
+            'comments':'Santa cat',
+            'info':'Posted by Richard on Monday'
+        }, 
+        {
+            'id':3,
+            'url':'https://25.media.tumblr.com/tumblr_m3rmbwhVB51qhwmnpo1_1280.jpg',
+            'comments':'Mexico Cat',
+            'info':'Posted by Richard on Monday'
+        },
+        {
+            'id':4,
+            'url':'https://24.media.tumblr.com/tumblr_mceknxs4Lo1qd477zo1_500.jpg',
+            'comments':'Curious Cat',
+            'info':'Posted by Richard on Monday'
+        }
+ 
+    ];
+    }
+        
+    
+}
+
+
